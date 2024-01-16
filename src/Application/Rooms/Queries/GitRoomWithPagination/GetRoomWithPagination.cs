3ab53b0c97e00d17d7187136a8e82de5294c6724
MengHuaH <255468864@qq.com>
2024-01-16 14:56:48 +0800

    添加RoomWithPagination

---
+++ src/Application/Rooms/Queries/GitRoomWithPagination/GetRoomWithPagination.cs
@@ -0,0 +1,34 @@
+﻿using spacesApi.Application.Common.Interfaces;
+using spacesApi.Application.Common.Mappings;
+using spacesApi.Application.Common.Models;
+using spacesApi.Domain.Entities;
+
+namespace spacesApi.Application.Rooms.Queries.GetRoomWithPagination;
+
+public record GetRoomWithPaginationQuery : IRequest<PaginatedList<Room>>
+{
+    public int Id { get; init; }
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
+public class GetRoomWithPaginationQueryHandler : IRequestHandler<GetRoomWithPaginationQuery, PaginatedList<Room>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetRoomWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<Room>> Handle(GetRoomWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Room
+            .Where(x => x.Id == request.Id)
+            .OrderBy(x => x.Id)
+            .ProjectTo<Room>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+    }
+}
---
+++ src/Application/Rooms/Queries/GitRoomWithPagination/GetRoomWithPaginationQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace spacesApi.Application.Rooms.Queries.GetRoomWithPagination;
+
+public class GetRoomWithPaginationQueryValidator : AbstractValidator<GetRoomWithPaginationQuery>
+{
+    public GetRoomWithPaginationQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("ListId is required.");
+
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(1).WithMessage("PageNumber at least greater than or equal to 1.");
+
+        RuleFor(x => x.PageSize)
+            .GreaterThanOrEqualTo(1).WithMessage("PageSize at least greater than or equal to 1.");
+    }
+}
