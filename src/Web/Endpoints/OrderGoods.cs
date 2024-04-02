89e95cc73bba9bbafd828aa9ba7c7bfa7a6e89ca
MengHuaH <255468864@qq.com>
2024-04-02 17:53:51 +0800

    订单接口修改

---
+++ src/Web/Endpoints/OrderGoods.cs
@@ -0,0 +1,37 @@
+﻿using spacesApi.Application.Common.Models;
+using spacesApi.Application.OrderGoodss.Queries.GetOrderGoodsWithPagination;
+using spacesApi.Application.OrderGoodss.Commands.CreateOrderGoods;
+using spacesApi.Application.OrderGoodss.Commands.DeleteOrderGoods;
+using spacesApi.Application.OrderGoodss.Queries.GitOrderGoodsWithPagination;
+
+
+namespace spacesApi.Web.Endpoints;
+
+public class OrderGoods : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            //.RequireAuthorization()
+            .MapGet(GetOrderGoodsWithPagination)
+            .MapPost(CreateOrderGoods)
+            .MapDelete(DeleteOrderGoods, "{id}");
+    }
+
+    public async Task<PaginatedList<OrderGoodsListDto>> GetOrderGoodsWithPagination(ISender sender, [AsParameters] GetOrderGoodsWithPaginationQuery query)
+    {
+        return await sender.Send(query);
+    }
+
+    public async Task<int> CreateOrderGoods(ISender sender, CreateOrderGoodsCommand command)
+    {
+        return await sender.Send(command);
+    }
+
+
+    public async Task<IResult> DeleteOrderGoods(ISender sender, int id)
+    {
+        await sender.Send(new DeleteOrderGoodsCommand(id));
+        return Results.NoContent();
+    }
+}
